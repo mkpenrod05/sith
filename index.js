0b7daa44b7a10c8045e2ad59eb013f9749db52d1
Michael Penrod <michael.penrod@vivint.com>
2015-09-30 18:17:23 -0600

    MORE FACES!!

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.get('/', function(request, response) {
 });
 
 app.get('/cool', function(request, response) {
-  response.send(cool());
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+    result += cool();
+  response.send(result);
 });
 
 app.listen(app.get('port'), function() {
